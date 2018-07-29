1e541cb6fcb8888c5290fb399f29f59539b58a9b
ashvinikumar <ashvini.kumar@highpeaksw.com>
2018-07-30 00:06:06 +0530

    lambda

---
+++ function/lambdaFunc/map_lambda.py
@@ -0,0 +1,23 @@
+# Create a list of strings: spells
+spells = ["protego", "accio", "expecto patronum", "legilimens"]
+
+# Use map() to apply a lambda function over spells: shout_spells
+shout_spells = map(lambda item: item+'!!!', spells)
+
+# Convert shout_spells to a list: shout_spells_list
+shout_spells_list = list(shout_spells)
+
+# Convert shout_spells into a list and print it
+print(shout_spells_list)
+
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'pippin', 'aragorn', 'boromir', 'legolas', 'gimli', 'gandalf']
+
+# Use filter() to apply a lambda function over fellowship: result
+result = filter(lambda x: len(x) > 6, fellowship)
+
+# Convert result to a list: result_list
+result_list = list(result)
+
+# Convert result into a list and print it
+print(result_list)
