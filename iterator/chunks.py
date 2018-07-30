1cfd348019096edb9ef637cc45e094f0c633348c
ashvinikumar <ashvini.kumar@highpeaksw.com>
2018-07-30 17:17:20 +0530

    added loop AND Iterator

---
+++ iterator/chunks.py
@@ -0,0 +1,18 @@
+import pandas as pd
+# Initialize an empty dictionary: counts_dict
+
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Iterate over the file chunk by chunk
+for chunk in pd.read_csv('/Users/ashvinikumar/PycharmProjects/ML/data_science/tweets.csv', chunksize=10):
+
+    # Iterate over the column in DataFrame
+    for entry in chunk['lang']:
+        if entry in counts_dict.keys():
+            counts_dict[entry] += 1
+        else:
+            counts_dict[entry] = 1
+
+# Print the populated dictionary
+print(counts_dict)
---
+++ iterator/iter_exp.py
@@ -0,0 +1,56 @@
+# Create a list of strings: flash
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
+# Print each list item in flash using a for loop
+
+for var in flash:
+    print(var)
+
+# Create an iterator for flash: superspeed
+superspeed = iter(flash)
+
+# Print each item from the iterator
+print(next(superspeed))
+print(next(superspeed))
+print(next(superspeed))
+print(next(superspeed))
+
+# Create a range object: values
+values = range(10,21)
+
+# Print the range object
+print(values)
+
+# Create a list of integers: values_list
+values_list = list(values)
+
+# Print values_list
+print(values_list)
+
+# Get the sum of values: values_sum
+values_sum = sum(values)
+
+# Print values_sum
+print(values_sum)
+
+
+# Create a list of strings: mutants
+mutants = ['charles xavier',
+            'bobby drake',
+            'kurt wagner',
+            'max eisenhardt',
+            'kitty pryde']
+
+# Create a list of tuples: mutant_list
+mutant_list = list(enumerate(mutants))
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for index1, value1 in mutant_list:
+    print(index1, value1)
+
+# Change the start index
+for index2, value2 in list(enumerate(mutants, start=1)):
+    print(index2, value2)
